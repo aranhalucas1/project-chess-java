64edb4cbd613da7edcb22dc7a1bedec3d490f00f
aranhalucas1 <62998019+aranhalucas1@users.noreply.github.com>
2020-05-29 20:39:19 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -46,6 +46,19 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("The selected position does not exist");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	public boolean positionExists(Integer row, Integer column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
