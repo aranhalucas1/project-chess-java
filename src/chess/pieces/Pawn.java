f210aa64159d0f1e7f0f0c3b82761c84c5056870
aranhalucas1 <21315381@anhembimorumbi.edu.br>
2020-06-02 17:07:38 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -35,20 +35,19 @@ public class Pawn extends ChessPiece {
 					&& !getBoard().thereIsAPiece(p) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-		
-		// sw
-		p.setValues(position.getRow() - 1, position.getColumn() - 1);
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
 
-		// nw
-		p.setValues(position.getRow() - 1, position.getColumn() + 1);
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		} 
-	}
-		else {
+			// sw
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// nw
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		} else {
 			p.setValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
